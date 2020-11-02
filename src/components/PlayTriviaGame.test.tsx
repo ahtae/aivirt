07dd97d76d5d486170a37d7d117c7c66fb56f6a2
Kristy Li <kristyli20.0@gmail.com>
2020-11-01 23:55:42 -0500

    test: Add tests to PlayTriviaGame component

---
+++ src/components/PlayTriviaGame.test.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+import PlayTriviaGame from './PlayTriviaGame';
+// import { shallow } from 'enzyme';
+
+describe('PlayTriviaGame Component', () => {
+  const score = 0;
+  const increaseScore = jest.fn();
+  const setHasClickedGetStartedButton = jest.fn();
+  const resetScore = jest.fn();
+
+  const component = render(
+    <PlayTriviaGame
+      score={score}
+      increaseScore={increaseScore}
+      setHasClickedGetStartedButton={setHasClickedGetStartedButton}
+      resetScore={resetScore}
+    />
+  );
+  const getStartedButton = component.container.querySelector(
+    '#get-started-button'
+  );
+  const resetButton = component.container.querySelector('#reset-button');
+  const endGameButton = component.container.querySelector('#end-game-button');
+  const checkButton = component.container.querySelector('#check-button');
+  const nextButton = component.container.querySelector('#next-button');
+  const triviaDiv = component.container.querySelector('#trivia');
+
+  test('renders content', () => {
+    expect(getStartedButton).toBe(null);
+    expect(triviaDiv).not.toBe(null);
+    expect(resetButton).toHaveTextContent('reset');
+    expect(checkButton).toHaveTextContent('check');
+    expect(nextButton).toHaveTextContent('next');
+    expect(endGameButton).toHaveTextContent('end game');
+  });
+});
