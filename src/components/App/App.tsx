054c76ba2dcb9771ca8d60b687e0f0a1426cbef3
Kristy Li <kristyli20.0@gmail.com>
2020-11-07 00:43:05 -0500

    chore: Pass trivia data as a prop for the Home component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import { Home } from '../index';
+import { Trivia } from '../../types';
+import trivia from '../../data/trivia.json';
+import testTrivia from '../../data/trivia.test.json';
 import './App.css';
 
-const App: React.FC = () => <Home />;
+let listOfTrivia: Array<Trivia>;
+
+if (process.env.NODE_ENV === 'test') {
+  listOfTrivia = testTrivia;
+} else {
+  listOfTrivia = trivia;
+}
+
+const App: React.FC = () => <Home listOfTrivia={listOfTrivia} />;
 
 export default App;
