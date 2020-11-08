7156cacda48214e5fdee6ca90d7e27f6bd92f0e1
Kristy Li <kristyli20.0@gmail.com>
2020-11-08 11:08:07 -0500

    chore: Import and export ReviewTriviaQuestions and ReviewTriviaQuestion

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,9 +3,13 @@ import Home from './Home/Home';
 import PlayTriviaGame from './PlayTriviaGame/PlayTriviaGame';
 import Results from './Results/Results';
 import TriviaCard from './TriviaCard/TriviaCard';
+import ReviewTriviaQuestions from './ReviewTriviaQuestions/ReviewTriviaQuestions';
+import ReviewTriviaQuestion from './ReviewTriviaQuestions/ReviewTriviaQuestion/ReviewTriviaQuestion';
 
 export { App };
 export { Home };
 export { PlayTriviaGame };
 export { Results };
 export { TriviaCard };
+export { ReviewTriviaQuestions };
+export { ReviewTriviaQuestion };
